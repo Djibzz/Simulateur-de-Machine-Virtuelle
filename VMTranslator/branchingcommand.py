ac175c56027371b6797f7f32a9c80826661c1e67
Rayaleh Keynane <keynane.rayaleh@etu.univ-orleans.fr>
2024-10-11 15:36:16 +0200

    debut de restructuration du code

---
+++ VMTranslator/branchingcommand.py
@@ -0,0 +1,32 @@
+class BranchingCommand:
+    def asm(self,command):
+        match command['type']:
+            case 'label':
+                return self._commandlabel(command)
+            case 'goto':
+                return self._commandgoto(command)
+            case 'ifgoto':
+                return self._commandifgoto(command)
+
+
+def _commandlabel(self, command):
+    return f""" //label {command['label']}
+    ({command['label']})
+    """
+
+
+def _commandgoto(self, command):
+    return f""" //goto {command['label']}
+    @{command['label']}
+    0;JMP
+    """
+
+
+def _commandifgoto(self, command):
+    return f""" //if goto {command['label']}
+    @SP
+    AM=M-1
+    D=M
+    @{command['label']}
+    D;JNE
+    """
